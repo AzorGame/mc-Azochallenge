fdb03d3f688bfa920b66e85ffc66c7005df4f080
BuildTools <unconfigured@null.spigotmc.org>
2020-05-10 20:44:03 +0200

    Created initial project.

---
+++ src/main/java/io/github/azorgame/azochallenge/AzoChallenge.java
@@ -0,0 +1,16 @@
+package io.github.azorgame.azochallenge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AzoChallenge extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
